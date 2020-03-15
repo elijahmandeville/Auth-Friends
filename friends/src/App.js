06177f951949076d23b7d7db4092d8272f8ae535
elijahmandeville <elijahmandeville@gmail.com>
2020-03-15 17:20:36 -0600

    initial project setup

---
+++ friends/src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import { Route, Link, Switch } from "react-router-dom";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>App Content</h1>
+    </div>
+  );
+}
+
+export default App;
